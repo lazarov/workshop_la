1769df37c292a8b4d30a79e35d9ba3795420d7be
la <lazarov@gazeta.pl>
2015-03-01 19:18:38 +0100

    sunday toshiba

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,45 +16,11 @@ Review.destroy_all
 
 # Seed Users & Category
 
-#User.create!(firstname: 'Jozef', lastname: 'Pilsudski', email: 'wodznr1@pl.pl', admin: true, encrypted_password: 'password')
-#User.create!(firstname: 'Adam', lastname: 'Kot', email: 'akot@kot.pl', password: 'password')
+User.create!(firstname: 'Jan', lastname: 'Pies', email: 'jpies@pl.pl', admin: true, password: 'password')
+User.create!(firstname: 'Adam', lastname: 'Kot', email: 'akot@kot.pl', password: 'password')
+User.create!(firstname: 'Tomek', lastname: 'Kruk', email: 'tkruk@kot.pl', password: 'password')
 
-# 7.times do
-#   user = User.create!(firstname: Faker::Name.first_name,
-#                        lastname: Faker::Name.last_name,
-#                           email: Faker::Internet.email,
-#                        password: Faker::Internet.password(8))
-#
-#   #puts user.inspect
-#   category = Category.create!( name: Faker::Product.brand )
-#   #puts category.inspect
-# end
-#
-# # Seed Products
-#
-# c_id = Category.order(id: :desc).limit(5).pluck(:id)
-# 30.times do
-#   product = Product.create!( title: Faker::Product.product_name,
-#                       description: Faker::Lorem.paragraph,
-#                             price: rand(1..99),
-#                       category_id: c_id[rand(c_id.length)])
-#
-#     #puts product.inspect
-# end
-#
-# # Seed Reviews
-#
-# p_id = Product.order(id: :desc).limit(5).pluck(:id)
-# u_id = User.order(id: :desc).limit(5).pluck(:id)
-# 100.times do
-#   review = Review.create!( content: Faker::Lorem.sentence,
-#                         product_id: p_id[rand(p_id.length)],
-#                             rating: rand(1..5),
-#                            user_id: u_id[rand(u_id.length)])
-#     #puts review.inspect
-# end
-
-7.times do
+8.times do
   user = User.create!(firstname: Faker::Name.first_name,
                        lastname: Faker::Name.last_name,
                           email: Faker::Internet.email,
@@ -64,13 +30,13 @@ Review.destroy_all
   #puts category.inspect
 
   if ( !category.nil? )
-    (rand(1..3)).times do
+    (rand(1..7)).times do
       product = category.products.create!(title: Faker::Product.product_name,
                         description: Faker::Lorem.paragraph,
                               price: rand(1..99))
 
       if ( !product.nil? && !user.nil? )
-        (rand(1..4)).times do
+        (rand(1..6)).times do
           review = product.reviews.create!(content: Faker::Lorem.sentence,
                                             rating: rand(1..5),
                                            user_id: user.id)
